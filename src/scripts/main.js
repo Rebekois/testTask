dac511d25290c55ecb07b98d3502aa8190a84b8c
Rebeko Ilia <Rebekois.hb@gmail.com>
2019-07-26 11:52:06 +1000

    First test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,13 @@
-window.onload{
-    let request = new XMLHttpRequest();
-    
+// ajaxGet()
 
 
+// function ajaxGet(){
+//     let requ = new XMLHttpRequest();
+//     requ.open('GET', 'http://www.mocky.io/v2/5d3979782f000057006ebb1a');
+//     requ.send();
+// }
 
-}
+
+let promise = $.ajax({
+    url:'http://www.mocky.io/v2/5944e07213000038025b6f30'
+})
